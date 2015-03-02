c025758b89feeb0273fc713e9b9342874148c78c
Atul Varma <varmaa@gmail.com>
2015-03-02 18:28:26 -0500

    add 's3' gulp task. fixes #11.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var path = require('path');
 var gulp = require('gulp');
+var s3 = require('gulp-s3');
+var gzip = require('gulp-gzip');
 var less = require('gulp-less');
 var prettify = require('gulp-prettify');
 var webpack = require('gulp-webpack');
@@ -8,6 +10,13 @@ require('node-jsx').install();
 
 var IndexFileStream = require('./gulp-index-file-stream');
 var webpackConfig = require('./webpack.config');
+
+var BUILD_TASKS = [
+  'copy-dirs',
+  'less',
+  'webpack',
+  'generate-index-files'
+];
 
 gulp.task('copy-dirs', function() {
   return gulp.src([
@@ -39,6 +48,27 @@ gulp.task('generate-index-files', function() {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', function() {
-  gulp.start('copy-dirs', 'less', 'webpack', 'generate-index-files');
+gulp.task('default', BUILD_TASKS);
+
+gulp.task('s3', BUILD_TASKS, function() {
+  var key = process.env.AWS_ACCESS_KEY;
+  var secret = process.env.AWS_SECRET_KEY;
+
+  if (!key || !secret)
+    throw new Error('Please set AWS_ACCESS_KEY and AWS_SECRET_KEY ' +
+                    'in your environment.');
+
+  return gulp.src('./dist/**')
+    .pipe(gzip())
+    .pipe(s3({
+      key: key,
+      secret: secret,
+      bucket: process.env.AWS_BUCKET || 'mozilla-learning-html',
+      region: process.env.AWS_REGION || 'us-east-1'
+    }, {
+      gzippedOnly: true,
+      headers: {
+        'Cache-Control': 'max-age=600, public'
+      }
+    }));
 });
