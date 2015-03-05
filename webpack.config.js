e7e01ace3679ede964059c298f0ad2e9fa65e2df
Atul Varma <varmaa@gmail.com>
2015-03-05 18:39:54 -0500

    make tests work in phantom 1.9.x and other non-ES5 browsers.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 var webpack = require('webpack');
 
+var IMPORT_ES5_SHIM = "imports?shim=es5-shim/es5-shim&" +
+                      "sham=es5-shim/es5-sham";
+
 module.exports = {
   entry: {
     app: './lib/main.jsx',
@@ -12,7 +15,10 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.jsx$/, loader: "jsx-loader"}
+      {test: /\.jsx$/, loader: "jsx-loader"},
+      // https://github.com/webpack/webpack/issues/558#issuecomment-60889168
+      {test: require.resolve("react"), loader: IMPORT_ES5_SHIM},
+      {test: require.resolve("react/addons"), loader: IMPORT_ES5_SHIM}
     ]
   },
   plugins: [
