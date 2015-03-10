199cbba28a7769dc3b9a2deb12cd88bb04a04091
Aki Rose Braun <hi@akibraun.com>
2015-03-10 12:57:49 -0700

    Everything passes the lint-test!

--- test/config.test.js
+++ test/config.test.js
@@ -2,7 +2,7 @@ var should = require('should');
 
 var config = require('../lib/config');
 
-describe("config", function() {
+describe('config', function() {
   it('should think it is not in the browser', function() {
     config.IN_STATIC_SITE.should.be.false;
     config.GENERATING_STATIC_SITE.should.be.true;
