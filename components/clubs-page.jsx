820651cc2a349be7124aa06351baa3178ea8538f
Ricardo Vazquez <ricardo@mozillafoundation.org>
2015-03-12 17:17:03 -0400

    Design work on bottomCTA section

--- components/clubs-page.jsx
+++ components/clubs-page.jsx
@@ -107,7 +107,7 @@ var BottomCTA = React.createClass({
         <div className="col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-2 col-lg-8">
           <PageEndCTA linkTo={this.props.ctaLink} ctaBtnText="Add your club to the map">
             <img className="divider" src="/img/clubs-line-divider.svg" alt="" />
-            <p>Do you meet regularly with a group of learners to increaset web literacy skills?</p>
+            <p>Do you meet regularly with a group of learners to increase web literacy skills?</p>
           </PageEndCTA>
         </div>
       </div>
@@ -145,7 +145,7 @@ var ClubsPage = React.createClass({
         <section>
           <IconLinks/>
         </section>
-        <section>
+        <section className="bottomCTA">
           <BottomCTA ctaLink={theCtaLink}/>
         </section>
       </div>
