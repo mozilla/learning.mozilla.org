f92602e11fc75def6fc3eb185b09773081bd8b7d
Atul Varma <varmaa@gmail.com>
2015-03-16 13:29:03 -0400

    added 2 TeachAPI tests.

--- test/browser/teach-api.test.js
+++ test/browser/teach-api.test.js
@@ -4,19 +4,54 @@ var sinon = window.sinon;
 var TeachAPI = require('../../lib/teach-api');
 
 describe('TeachAPI', function() {
-  var xhr;
+  var xhr, requests, storage;
 
   beforeEach(function() {
+    requests = [];
     xhr = sinon.useFakeXMLHttpRequest();
+    xhr.onCreate = function(xhr) {
+      requests.push(xhr);
+    };
+    storage = {};
   });
 
   afterEach(function() {
     xhr.restore();
   });
 
-  it('should instantiate', function() {
-    var api = new TeachAPI();
+  describe('startLogin()', function() {
+    var personaCb;
 
+    beforeEach(function() {
+      personaCb = null;
+      window.navigator.id = {
+        get: function(cb) {
+          personaCb = cb;
+        }
+      };
+    });
 
+    afterEach(function() {
+      delete window.navigator.id;
+    });
+
+    it('does nothing if given no assertion', function() {
+      var api = new TeachAPI({storage: storage});
+
+      api.startLogin();
+      personaCb(null);
+      requests.should.eql([]);
+    });
+
+    it('emits error if navigator.id is falsy', function(done) {
+      var api = new TeachAPI({storage: storage});
+
+      delete window.navigator.id;
+      api.once('error', function(err) {
+        err.message.should.eql('navigator.id does not exist');
+        done();
+      });
+      api.startLogin();
+    });
   });
 });
