d6280a1ff515c4178b52d0ff2b29b612fb076a02
Scott <scott@localhost.localdomain>
2015-03-17 19:13:06 -0400

    review

--- components/hero-unit.jsx
+++ components/hero-unit.jsx
@@ -5,15 +5,14 @@ var HeroUnit = React.createClass({
     'image': React.PropTypes.string.isRequired
   },
   render: function() {
+    var image = this.props.image;
+    if (typeof window !== 'undefined' && window.devicePixelRatio > 1.5) {
+      image = this.props.image2x || this.props.image;
+    }
     return (
       <div className="row">
-        <div className="col-md-12 hero-unit hero-unit-1x" style={{
-          backgroundImage: 'url(' + this.props.image + ')'
-        }}>
-          {this.props.children}
-        </div>
-        <div className="col-md-12 hero-unit hero-unit-2x" style={{
-          backgroundImage: 'url(' + this.props.image2x || this.props.image + ')'
+        <div className="col-md-12 hero-unit" style={{
+          backgroundImage: 'url(' + image + ')'
         }}>
           {this.props.children}
         </div>
