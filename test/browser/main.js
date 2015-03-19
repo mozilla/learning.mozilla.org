a19d3b71355184a5707b4253f7ad43429784f7fb
Atul Varma <varmaa@gmail.com>
2015-03-19 18:04:08 -0400

    add test suite for TeachAPIClientMixin.

--- test/browser/main.js
+++ test/browser/main.js
@@ -4,5 +4,6 @@ require('./config.test.js');
 require('./sidebar.test.jsx');
 require('./imagetag.test.jsx');
 require('./teach-api.test.js');
+require('./teach-api-client.test.jsx');
 require('./login.test.jsx');
 require('./modal.test.jsx');
