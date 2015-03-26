2a3d62a4226149725a8b9c39327539055e2e1383
Atul Varma <varmaa@gmail.com>
2015-03-26 19:31:51 -0400

    Scroll map into view when taking users to their club on it.
    Fixes #382.

--- pages/clubs.jsx
+++ pages/clubs.jsx
@@ -431,6 +431,7 @@ var ClubsPage = React.createClass({
     this.showModal(ModalLearnMore);
   },
   handleAddClubSuccess: function(club) {
+    this.refs.map.getDOMNode().scrollIntoView();
     this.refs.map.focusOnClub(club);
   },
   handleClubDelete: function(url, clubName) {
