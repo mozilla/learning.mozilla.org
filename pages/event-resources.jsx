55de7525f78d8919ab2abb4e152cefff8838af97
Scott <scott@localhost.localdomain>
2015-04-03 22:19:40 -0400

    more work

--- pages/event-resources.jsx
+++ pages/event-resources.jsx
@@ -38,12 +38,12 @@ var EventItem = React.createClass({
 var LogoAsset = React.createClass({
   render: function() {
     return (
-      <div className="col-sm-3 col-md-3 col-lg-3">
-        <ImageTag
+      <div className="logo-asset-container col-sm-3 col-md-6 col-lg-3">
+        <ImageTag className="logo-asset-img"
         width={200} height={200}
         src1x={this.props.src1x} src2x={this.props.src2x}
         alt={this.props.alt}/>
-        <div>{this.props.children}</div>
+        <div className="logo-asset-hover">{this.props.children}</div>
         <div>{this.props.head}</div>
       </div>
     );
@@ -52,7 +52,14 @@ var LogoAsset = React.createClass({
 var LogoAssetLink = React.createClass({
   render: function() {
     return (
-      <a href={this.props.children}><span className="ion ion-ios-copy"></span>{this.props.children}</a>
+      <a className="logo-asset-link" href={this.props.href}><span className="ion ion-ios-copy"></span>{this.props.children}</a>
+    );
+  }
+});
+var RemixLink = React.createClass({
+  render: function() {
+    return (
+      <a className="remix-link" href={this.props.href}>Remix</a>
     );
   }
 });
@@ -113,52 +120,57 @@ var EventsResources = React.createClass({
 
         <div className="row">
           <LogoAsset head="Maker Party Logo"
-          src1x="/img/event-resources-page/MakerPartyLogo-thumb.png"
-          src2x="">
-            <LogoAssetLink href="">EPS</LogoAssetLink>
-            <LogoAssetLink href="">PNG</LogoAssetLink>
+          src1x="/img/event-resources-page/resource-thumbnails-01.png"
+          src2x="/img/event-resources-page/resource-thumbnails-01@2x.png">
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyLogo.eps">Download EPS</LogoAssetLink>
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyLogo.png">Download PNG</LogoAssetLink>
           </LogoAsset>
           <LogoAsset head="Brand Palette"
-          src1x="/img/event-resources-page/MakerPartyBanner-thumb.png"
-          src2x="">
-            <LogoAssetLink href="">EPS</LogoAssetLink>
-            <LogoAssetLink href="">PNG</LogoAssetLink>
+          src1x="/img/event-resources-page/resource-thumbnails-03.png"
+          src2x="/img/event-resources-page/resource-thumbnails-03@2x.png">
+            <LogoAssetLink href="">Download EPS</LogoAssetLink>
+            <LogoAssetLink href="">Download PNG</LogoAssetLink>
           </LogoAsset>
           <LogoAsset head="Poster"
-          src1x="/img/event-resources-page/MakerPartyBanner-thumb.png"
-          src2x="">
-            <LogoAssetLink href="">EPS</LogoAssetLink>
-            <LogoAssetLink href="">PNG</LogoAssetLink>
+          src1x="/img/event-resources-page/resource-thumbnails-07.png"
+          src2x="/img/event-resources-page/resource-thumbnails-07@2x.png">
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyBanner.eps">Download EPS</LogoAssetLink>
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyBanner.png">Download PNG</LogoAssetLink>
           </LogoAsset>
           <LogoAsset head="Desktop Wallpaper"
-          src1x="/img/event-resources-page/MakerPartyWallpaper-1-thumb.png"
-          src2x="">
-            <LogoAssetLink href="">EPS</LogoAssetLink>
-            <LogoAssetLink href="">PNG</LogoAssetLink>
+          src1x="/img/event-resources-page/resource-thumbnails-10.png"
+          src2x="/img/event-resources-page/resource-thumbnails-10@2x.png">
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyWallpaper-1-320x480.jpg">320px x 480px</LogoAssetLink>
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyWallpaper-1-640x1136.jpg">640px x 1136px</LogoAssetLink>
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyWallpaper-1-768x1280.jpg">768px x 1280px</LogoAssetLink>
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyWallpaper-1-1920x1200.jpg">1920px x 1200px</LogoAssetLink>
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyWallpaper-1-2560x1440.jpg">2560px x 1440px</LogoAssetLink>
           </LogoAsset>
+        </div>
+        <div className="row">
           <LogoAsset head="Certificate"
-          src1x="/img/event-resources-page/MakerPartyCertificateMake-thumb.png"
-          src2x="">
-            <LogoAssetLink href="">EPS</LogoAssetLink>
-            <LogoAssetLink href="">PNG</LogoAssetLink>
+          src1x="/img/event-resources-page/resource-thumbnails-08.png"
+          src2x="/img/event-resources-page/resource-thumbnails-08@2x.png">
+            <RemixLink href="https://makerparty2014.makes.org/thimble/LTEzNDM3NTAxNDQ=/maker-party-love-bomb">Download EPS</RemixLink>
           </LogoAsset>
           <LogoAsset head="Table Cloth Design"
-          src1x="/img/event-resources-page/MakerPartyTableClothDesign-thumb.png"
-          src2x="">
-            <LogoAssetLink href="">EPS</LogoAssetLink>
-            <LogoAssetLink href="">PNG</LogoAssetLink>
+          src1x="/img/event-resources-page/resource-thumbnails-04.png"
+          src2x="/img/event-resources-page/resource-thumbnails-04@2x.png">
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyTableClothDesign.eps">Download EPS</LogoAssetLink>
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyTableClothDesign.png">Download PNG</LogoAssetLink>
           </LogoAsset>
           <LogoAsset head="Buttons"
-          src1x="/img/event-resources-page/MakerPartyButtons-thumb.png"
-          src2x="">
-            <LogoAssetLink href="">EPS</LogoAssetLink>
-            <LogoAssetLink href="">PNG</LogoAssetLink>
+          src1x="/img/event-resources-page/resource-thumbnails-06.png"
+          src2x="/img/event-resources-page/resource-thumbnails-06@2x.png">
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyButtons.eps">Download EPS</LogoAssetLink>
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/MakerPartyButtons.png">Download PNG</LogoAssetLink>
           </LogoAsset>
           <LogoAsset head="KUMI Papertoy"
-          src1x="/img/event-resources-page/KUMI-Papertoy-blank-tees-thumb.png"
-          src2x="">
-            <LogoAssetLink href="">EPS</LogoAssetLink>
-            <LogoAssetLink href="">PNG</LogoAssetLink>
+          src1x="/img/event-resources-page/resource-thumbnails-09.png"
+          src2x="/img/event-resources-page/resource-thumbnails-09@2x.png">
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/KUMI-Papertoy-yellow.pdf">Yellow T-sirt</LogoAssetLink>
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/KUMI-Papertoy-blue.pdf">Teal T-shirt</LogoAssetLink>
+            <LogoAssetLink href="https://party.webmaker.org/party-resources/KUMI-Papertoy-blank-tees.pdf">Design Your Own T-shirt</LogoAssetLink>
           </LogoAsset>
         </div>
 
@@ -172,16 +184,24 @@ var EventsResources = React.createClass({
         <h2 id="event-support">Event Support</h2>
         <p>Get help from the community and our staff.</p>
 
-        <div className="row">
-          <div className="col-sm-6 col-md-6 col-lg-6">
-            <img></img>
+        <div className="event-support-container">
+          <div className="event-support-panel">
+            <ImageTag
+            width={399} height={254}
+            src1x="/img/event-resources-page/ask-the-community-image.png"
+            src2x="/img/event-resources-page/ask-the-community-image@2x.png"
+            alt="ask the community image"/>
             <h3 className="event-support-header">Ask The Community</h3>
             <p>
               We have a large, global community of people like you who have hosted events and have lots of advice to share. Visit our <a href="">discussion forum</a> to ask questions or share your own advice and experience with others.
             </p>
           </div>
-          <div className="col-sm-6 col-md-6 col-lg-6">
-            <img></img>
+          <div className="event-support-panel">
+            <ImageTag
+            width={399} height={254}
+            src1x="/img/event-resources-page/request-support-image.png"
+            src2x="/img/event-resources-page/request-support-image@2x.png"
+            alt="request support image"/>
             <h3 className="event-support-header">Request Support</h3>
             <p>
               Still can't find an answer to your question? Our team is here to help you with all things Maker Party. <a href="">Contact us</a> and we will get back to you as soon as possible.
