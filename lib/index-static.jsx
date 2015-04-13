4ec45a9e142fde6627ac054d18c4f87a97648fe8
Atul Varma <varmaa@gmail.com>
2015-04-13 18:52:14 -0400

    use protocol-relative URL for google fonts.

--- lib/index-static.jsx
+++ lib/index-static.jsx
@@ -34,7 +34,7 @@ function generateWithPageHTML(url, options, pageHTML) {
         <meta charSet="utf-8"/>
         <meta name="url" value={url}/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,700,600italic,700italic,800,800italic"/>
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,700,600italic,700italic,800,800italic"/>
         <link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css"/>
         <link href="https://mozorg.cdn.mozilla.net/media/css/tabzilla-min.css" rel="stylesheet" />
         <link rel="stylesheet" href={'/' + exports.CSS_FILENAME}/>
