6fbcb9a656499cdfbf133f2b93567125f27967f3
Jessica Klein <jessicabrookeklein@gmail.com>
2015-04-21 10:11:50 -0400

    removed button on home page
    Fixes https://github.com/mozilla/teach.webmaker.org/issues/746

--- pages/home.jsx
+++ pages/home.jsx
@@ -27,10 +27,6 @@ var HomePage = React.createClass({
                   image2x="/img/hero-unit@2x.png">
           <h1>Unlock opportunities for all citizens of the Web.</h1>
 
-          {process.env.SOFTEST_OF_LAUNCHES == 'on' ? null :
-          <div><Link to="join" className="btn btn-awsm">Start a Mozilla Club</Link></div>
-          }
-
         </HeroUnit>
         <div className="inner-container">
           <div className="values">
