b0573563e8ad0ca889b6821393a589bcaadbf20f
Atul Varma <varmaa@gmail.com>
2015-04-22 09:55:54 -0400

    add tests for setting document.title.

--- test/browser/page.test.jsx
+++ test/browser/page.test.jsx
@@ -14,29 +14,57 @@ var FakeModal = React.createClass({
 });
 
 describe("page", function() {
-  var handler, page, xhr;
+  var handler, page, xhr, originalTitle;
 
-  beforeEach(function(done) {
-    xhr = sinon.useFakeXMLHttpRequest();
-    Router.run(routes.routes, '/', function(Handler) {
+  function visitPage(url, cb) {
+    Router.run(routes.routes, url, function(Handler) {
       handler = TestUtils.renderIntoDocument(<Handler/>);
       page = TestUtils.findAllInRenderedTree(handler, function(c) {
         return !!c.showModal;
       })[0];
+      cb();
+    });
+  }
+
+  beforeEach(function() {
+    xhr = sinon.useFakeXMLHttpRequest();
+    originalTitle = document.title;
+    handler = null;
+    page = null;
+  });
+
+  afterEach(function() {
+    if (handler) {
+      React.unmountComponentAtNode(handler.getDOMNode().parentNode);
+    }
+    xhr.restore();
+    document.title = originalTitle;
+  });
+
+  it("adds body.modal-open when modal is visible", function(done) {
+    visitPage('/', function() {
+      document.body.className.should.not.match(/modal-open/);
+      page.showModal(FakeModal);
+      document.body.className.should.match(/modal-open/);
+      page.hideModal(FakeModal);
+      document.body.className.should.not.match(/modal-open/);
       done();
     });
   });
 
-  afterEach(function() {
-    React.unmountComponentAtNode(handler.getDOMNode().parentNode);
-    xhr.restore();
+  it("sets default page title when pageTitle is undefined", function(done) {
+    visitPage('/', function() {
+      page.componentDidUpdate(page.props, page.state);
+      document.title.should.equal("Mozilla Learning");
+      done();
+    });
   });
 
-  it("adds body.modal-open when modal is visible", function() {
-    document.body.className.should.not.match(/modal-open/);
-    page.showModal(FakeModal);
-    document.body.className.should.match(/modal-open/);
-    page.hideModal(FakeModal);
-    document.body.className.should.not.match(/modal-open/);
+  it("sets page title when pageTitle is a string", function(done) {
+    visitPage('/fixme/', function() {
+      page.componentDidUpdate(page.props, page.state);
+      document.title.should.equal("[FIXME] - Mozilla Learning");
+      done();
+    });
   });
 });
