4352d599c6ae049f43edbc79fa1309375c138022
Mavis Ou <mmmavisss@gmail.com>
2015-04-28 11:15:27 -0700

    Fixes #813 - fixed missing links and thumbnail

--- pages/event-resources.jsx
+++ pages/event-resources.jsx
@@ -389,7 +389,7 @@ var EventsResources = React.createClass({
             alt="ask the community image"/>
             <h3 className="event-support-header">Ask The Community</h3>
             <p>
-              We have a large, global community of people like you who have hosted events and have lots of advice to share. Visit our <a href="">discussion forum</a> to ask questions or share your own advice and experience with others.
+              We have a large, global community of people like you who have hosted events and have lots of advice to share. Visit our <a href="http://discourse.webmaker.org/category/maker-party">discussion forum</a> to ask questions or share your own advice and experience with others.
             </p>
           </div>
           <div className="event-support-panel">
@@ -400,7 +400,7 @@ var EventsResources = React.createClass({
             alt="request support image"/>
             <h3 className="event-support-header">Request Support</h3>
             <p>
-              Still can't find an answer to your question? Our team is here to help you with all things Maker Party. <a href="">Contact us</a> and we will get back to you as soon as possible.
+              Still can't find an answer to your question? Our team is here to help you with all things Maker Party. <a href="mailto:teachtheweb@mozillafoundation.org">Contact us</a> and we will get back to you as soon as possible.
             </p>
           </div>
         </div>
