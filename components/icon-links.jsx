c07504ec5db673f62405d5f5d5d5b2741da4da15
Mavis Ou <mmmavisss@gmail.com>
2015-05-14 13:17:22 -0700

    raise console.warn if num of IconLink is not 2 nor 3

--- components/icon-links.jsx
+++ components/icon-links.jsx
@@ -1,15 +1,21 @@
 var React = require('react');
 
 var IconLinks = React.createClass({
+  componentDidMount: function() {
+    var numChild = this.props.children.length;
+    if (process.env.NODE_ENV !== 'PRODUCTION' && (numChild !== 2 && numChild !== 3) ) {
+      console.error("We should only have 2 or 3 icons in a IconLinks. Otherwise please assign different classes to wrap around <IconLink> component.");
+    }
+  },
   render: function() {
-    var links = this.props.children;
+    var links = Array.isArray(this.props.children) ? this.props.children : [this.props.children];
     var classes;
     return (
       <div className="row icon-links">
         {
           links.map(function(link, i){
             // we either have 2 or 3 icons in a IconLinks row
-            classes = (links.length === 2 & i === 0) ?
+            classes = (links.length === 2 && i === 0) ?
                       "col-sm-4 col-md-4 col-lg-4 col-sm-offset-2 col-md-offset-2 col-lg-offset-2" :
                       "col-sm-4 col-md-4 col-lg-4";
             return(
