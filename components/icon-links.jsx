dba9ec5250b18bc66b68a5e4f46919880ee7842e
Mavis Ou <mmmavisss@gmail.com>
2015-05-13 21:47:04 -0700

    Fixes #897 - removed ref to 'Add your event'

--- components/icon-links.jsx
+++ components/icon-links.jsx
@@ -2,8 +2,22 @@ var React = require('react');
 
 var IconLinks = React.createClass({
   render: function() {
+    var links = this.props.children;
+    var classes;
     return (
-      <div className="row icon-links">{this.props.children}</div>
+      <div className="row icon-links">
+        {
+          links.map(function(link, i){
+            // we either have 2 or 3 icons in a IconLinks row
+            classes = (links.length === 2 & i === 0) ?
+                      "col-sm-4 col-md-4 col-lg-4 col-sm-offset-2 col-md-offset-2 col-lg-offset-2" :
+                      "col-sm-4 col-md-4 col-lg-4";
+            return(
+              <div className={classes} key={i}>{link}</div>
+            )
+          })
+        }
+      </div>
     );
   }
 });
