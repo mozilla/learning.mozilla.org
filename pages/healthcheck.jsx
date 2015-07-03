184f72fa02c575746ac1ea50ad798d45c5b9dcab
Mavis Ou <mmmavisss@gmail.com>
2015-07-03 16:48:11 -0700

    Fixes #884 - add healthcheck page

---
+++ pages/healthcheck.jsx
@@ -0,0 +1,43 @@
+var React = require('react');
+var packageJSON = require('../package.json');
+
+var HealthcheckPage = React.createClass({
+  statics: {
+    pageClassName: "healthcheck",
+    pageTitle: 'Site Health Check'
+  },
+  getInitialState: function() {
+    return {
+      commit: ""
+    }
+  },
+  componentDidMount: function() {
+    var rev = document.querySelector('meta[name="git-rev"]').getAttribute('content');
+    var commitDom = document.querySelector(".commit");
+    commitDom.innerHTML = rev.slice(0,10);
+    commitDom.setAttribute("href", "https://github.com/mozilla/teach.webmaker.org/commit/"+rev);
+  },
+  render: function() {
+    var siteType = (process.env.NODE_ENV === 'production') ? "production" : "development";
+    return (
+      <div className="site-meta">
+        <h1>Site Health Check</h1>
+        <p>
+          This is a {siteType} version of the <a href="https://github.com/mozilla/teach.webmaker.org" target="_blank">Teach Site</a> based on commit
+          <code>
+              <a target="_blank" href="" className="commit"> </a>
+          </code>,
+          which is based on version
+          <code>
+            <a target="_blank"
+               href={"https://github.com/mozilla/teach.webmaker.org/releases/tag/v" + packageJSON.version}>
+              {packageJSON.version}
+            </a>
+          </code>
+        </p>
+      </div>
+    );
+  }
+});
+
+module.exports = HealthcheckPage;
