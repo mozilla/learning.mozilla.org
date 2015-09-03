4eba8893af04df603398df84fd531920174c184a
Mavis Ou <mmmavisss@gmail.com>
2015-09-02 18:04:26 -0700

    removed unused code

--- pages/clubs.jsx
+++ pages/clubs.jsx
@@ -2,7 +2,6 @@ var _ = require('underscore');
 var React = require('react/addons');
 var Router = require('react-router');
 var Link = Router.Link;
-var ga = require('react-ga');
 
 var HeroUnit = require('../components/hero-unit.jsx');
 var Map = require('../components/map.jsx');
