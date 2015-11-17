10ccc9329ec0063b9ea18e525df41647aa6d02cb
abualkarmi <abualkarmi@gmail.com>
2015-11-17 16:06:24 +0200

    Test Badges Page

--- pages/badges.jsx
+++ pages/badges.jsx
@@ -6,8 +6,10 @@ var IconLink = require('../components/icon-link.jsx');
 var HeroUnit = require('../components/hero-unit.jsx');
 var Link = require('react-router').Link;
 var OutboundLink = require('react-ga').OutboundLink;
-
+var _ = require('underscore');
+var BadgeIcon = require('../components/badge-icon.jsx');
 var CredlyInterface = require('../lib/credly');
+
 
 var accesstoken = "d56b47dc5f3b23abf1a8fc30b7d546464c1e73dc7b2a2683b4e385a719b28958d25f6f954f352a3eb615c953b4f66ca1aef32520f506b032b10996e97be18451";
 
@@ -43,12 +45,96 @@ var BadgePage = React.createClass({
         return data;
     },
     render: function() {
+
+        var data = [
+            {
+                title: 'Problem Solving',
+                status: 'achieved',
+                description: '',
+                icon: '/img/components/badge-icon/ProblemSolving.png',
+                icon2x: '/img/components/badge-icon/ProblemSolving@2x.png'
+            },
+            {
+                title: 'Collaboration',
+                status: 'pending',
+                description: 'Lorem ipsum dolor sit amet',
+                icon: '/img/components/badge-icon/Collaborative.png',
+                icon2x: '/img/components/badge-icon/Collaborative@2x.png'
+            },
+            {
+                title: 'Creativity',
+                status: 'eligible',
+                description: 'Lorem ipsum dolor sit amet',
+                icon: '/img/components/badge-icon/Creativity.png',
+                icon2x: '/img/components/badge-icon/Creativity@2x.png'
+            },
+            {
+                title: 'Creativity',
+                status: 'eligible',
+                description: 'Lorem ipsum dolor sit amet',
+                icon: '/img/components/badge-icon/Creativity.png',
+                icon2x: '/img/components/badge-icon/Creativity@2x.png'
+            },
+            {
+                title: 'Problem Solving',
+                status: 'achieved',
+                description: '',
+                icon: '/img/components/badge-icon/ProblemSolving.png',
+                icon2x: '/img/components/badge-icon/ProblemSolving@2x.png'
+            },
+            {
+                title: 'Creativity',
+                status: 'eligible',
+                description: 'Lorem ipsum dolor sit amet',
+                icon: '/img/components/badge-icon/Creativity.png',
+                icon2x: '/img/components/badge-icon/Creativity@2x.png'
+            },
+            {
+                title: 'Creativity',
+                status: 'eligible',
+                description: 'Lorem ipsum dolor sit amet',
+                icon: '/img/components/badge-icon/Creativity.png',
+                icon2x: '/img/components/badge-icon/Creativity@2x.png'
+            },
+            {
+                title: 'Problem Solving',
+                status: 'achieved',
+                description: '',
+                icon: '/img/components/badge-icon/ProblemSolving.png',
+                icon2x: '/img/components/badge-icon/ProblemSolving@2x.png'
+            }
+        ];
+
         return (
             <div>
                 <HeroUnit>
                     <h1>Badges</h1>
                 </HeroUnit>
-                { (this.state.badges ? this.state.badges : null)}
+
+                <div className="inner-container badges-content">
+                    <section>
+                        <p className="text-center">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid laborum, sed? Atque deleniti, excepturi itaque iusto labore laborum libero mollitia necessitatibus nihil non officia quia quisquam similique? Deserunt, quasi, reprehenderit.
+                        </p>
+
+                        <div className="row">
+                            { data.map(function(badge){
+                                    return (
+                                        <div className="col-md-4">
+                                            <BadgeIcon
+                                                icon={badge.icon}
+                                                icon2x={badge.icon2x}
+                                                title={badge.title}
+                                                status={badge.status}
+                                                alt={badge.title}
+                                                description={badge.description} />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </section>
+                </div>
             </div>
         );
     }
