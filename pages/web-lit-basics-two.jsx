c479bd32eb75ef59d89d571b82f847117c9cdfcb
Mavis Ou <mmmavisss@gmail.com>
2015-11-25 15:43:08 -0800

    add Web Literacy Basics II to activities page

--- pages/web-lit-basics-two.jsx
+++ pages/web-lit-basics-two.jsx
@@ -14,9 +14,8 @@ var CurriculumIntro = React.createClass({
           <Illustration
           height={204} width={204}
           src1x="/img/pages/web-lit-basics-two/intro.jpg"
-          src2x="/img/pages/web-lit-basics-two/intro.jpg"
           className="illustration-img-circle"
-          caption={<span>cc-by-2.0 Mozilla Festival</span>}>
+          caption={<a href="https://c2.staticflickr.com/6/5760/22431506387_43e85e71bd.jpg">cc-by-2.0 Mozilla Festival</a>}>
             <h2>Dive deeper into the basics of composing webpages, designing for accessibility, sharing resources, using online media, and working open.</h2>
           </Illustration>
         </section>
