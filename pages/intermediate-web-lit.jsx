76e9d36447f73d89ed1ac3327f2a265e1860cd04
hannahkane <hannah@mozillafoundation.org>
2016-03-09 09:45:52 -0800

    makes keywords bold

--- pages/intermediate-web-lit.jsx
+++ pages/intermediate-web-lit.jsx
@@ -40,7 +40,7 @@ var curriculumList = [
         originalImgSrc: "http://chadsansing.github.io/curriculum-testing/intermediate-web-lit-two/blue-pyramid.png",
         caption: "CC0, public domain by ClkerFreeVectorImages",
         subtitle: "Coding/Scripting, Composing, Design",
-        description: "Learners will use CSS to change the size of text on an HTML webpage, learning about coding, composing, and design. ",
+        description: "Learners will use CSS to change the size of text on an HTML webpage, learning about <strong>coding</strong>, <strong>composing</strong>, and <strong>design</strong>.",
         link: "https://chadsansing.github.io/curriculum-testing/curriculum-migration/intermediate-web-lit-css-one-migrated/session01-css-word-pyramid.html"
       },
       {
@@ -49,7 +49,7 @@ var curriculumList = [
         originalImgSrc: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Plantin_letterpress.jpg/320px-Plantin_letterpress.jpg",
         caption: "CC-BY-SA 3.0, France3470",
         subtitle: "Coding/Scripting, Composing, Design",
-        description: "Learners will use webfonts to create moods and tones on webpages, learning about coding, composing, and design.",
+        description: "Learners will use webfonts to create moods and tones on webpages, learning about <strong>coding</strong>, <strong>composing</strong>, and <strong>design</strong>.",
         link: "https://chadsansing.github.io/curriculum-testing/curriculum-migration/intermediate-web-lit-css-one-migrated/session02-css-fonts.html"
       },
       {
@@ -58,7 +58,7 @@ var curriculumList = [
         originalImgSrc: "https://c1.staticflickr.com/3/2224/2271837503_756e2260b5_o.jpg",
         caption: "CC-BY 2.0, L.E. Spry",
         subtitle: "Coding/Scripting, Composing, Design",
-        description: "Jump right into this Thimble project to improve the fonts on this remixable movie poster, learning about coding, composing, and design.",
+        description: "Jump right into this Thimble project to improve the fonts on this remixable movie poster, learning about <strong>coding</strong>, <strong>composing</strong>, and <strong>design</strong>.",
         link: "https://d157rqmxrxj6ey.cloudfront.net/chadsansing/22265/ "
       },
       {
@@ -67,7 +67,7 @@ var curriculumList = [
         originalImgSrc: "http://res.freestockphotos.biz/pictures/9/9201-blurred-colored-lights-pv.jpg",
         caption: "CC0, Petr Kratochvil",
         subtitle: "Coding/Scripting, Composing, Design",
-        description: "Learners will search for webfonts to include in a webpage, matching the mood, purpose, tone and voice of the page while learning about coding, composing, and design.",
+        description: "Learners will search for webfonts to include in a webpage, matching the mood, purpose, tone and voice of the page while learning about <strong>coding</strong>, <strong>composing</strong>, and <strong>design</strong>.",
         link: "https://chadsansing.github.io/curriculum-testing/curriculum-migration/intermediate-web-lit-css-one-migrated/session03-css-circles.html"
       },
       {
@@ -76,7 +76,7 @@ var curriculumList = [
         originalImgSrc: "http://chadsansing.github.io/curriculum-testing/intermediate-web-lit-two/blocks.png",
         caption: "CC0, Counseling",
         subtitle: "Coding/Scripting, Composing, Design",
-        description: "Learners will search for webfonts to include in a webpage, matching the mood, purpose, tone and voice of the page while learning about coding, composing, and design.",
+        description: "Learners will search for webfonts to include in a webpage, matching the mood, purpose, tone and voice of the page while learning about <strong>coding</strong>, <strong>composing</strong>, and <strong>design</strong>.",
         link: "https://chadsansing.github.io/curriculum-testing/curriculum-migration/intermediate-web-lit-css-one-migrated/session04-css-building-blocks.html"
       },
       {
@@ -85,7 +85,7 @@ var curriculumList = [
         originalImgSrc: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Ubosoth_decoration.jpg/320px-Ubosoth_decoration.jpg",
         caption: "CC-BY-SA 3.0, Michael Janich",
         subtitle: "Coding/Scripting, Composing, Design",
-        description: "Learners will apply text- and box-effect CSS styling techniques to inline and block elements on an HTML webpage, learning about coding, composing, and design.",
+        description: "Learners will apply text- and box-effect CSS styling techniques to inline and block elements on an HTML webpage, learning about <strong>coding</strong>, <strong>composing</strong>, and <strong>design</strong>.",
         link: "https://chadsansing.github.io/curriculum-testing/curriculum-migration/intermediate-web-lit-css-one-migrated/session05-css-decoration.html"
       },
       {
@@ -94,7 +94,7 @@ var curriculumList = [
         originalImgSrc: "http://chadsansing.github.io/curriculum-testing/intermediate-web-lit-two/webotron-head.png",
         caption: "CC-BY, Mozilla",
         subtitle: "Coding/Scripting, Composing, Design",
-        description: "Learners will apply CSS layout techniques to inline and block elements on an HTML webpage, learning about coding, composing, and design.",
+        description: "Learners will apply CSS layout techniques to inline and block elements on an HTML webpage, learning about <strong>coding</strong>, <strong>composing</strong>, and <strong>design</strong>.",
         link: "https://chadsansing.github.io/curriculum-testing/curriculum-migration/intermediate-web-lit-css-one-migrated/session06-webotron-and-the-scrambled-divs.html"
       },
       {
@@ -103,7 +103,7 @@ var curriculumList = [
         originalImgSrc: "http://chadsansing.github.io/curriculum-testing/intermediate-web-lit-two/flexbox-froggy-screen-cap-1.png",
         caption: "CC-BY, Mozilla",
         subtitle: "Coding/Scripting, Composing, Design",
-        description: "Learners will apply multi-part CSS selectors and flexbox positioning with external CSS tutorial games, learning about coding, composing, and design.",
+        description: "Learners will apply multi-part CSS selectors and flexbox positioning with external CSS tutorial games, learning about <strong>coding</strong>, <strong>composing</strong>, and <strong>design</strong>.",
         link: "https://chadsansing.github.io/curriculum-testing/curriculum-migration/intermediate-web-lit-css-one-migrated/session07-external-css-tutorials.html"
       },
     ]
