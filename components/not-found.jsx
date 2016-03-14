9ce03736e4678a947e92ea9bed971d941bb84f30
Mavis Ou <mmmavisss@gmail.com>
2016-03-14 15:30:14 -0700

    forgot to commit files

---
+++ components/not-found.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ImageTag = require('./imagetag.jsx');
+
+var NotFoundMessage = function(props) { 
+  return (
+    <div className="not-found">
+      <ImageTag width={500}
+                height='auto'
+                src1x='/img/pages/not-found/book_singlepageflip.gif'
+                alt='' />
+      <h2>Hey, this 404 is a teachable moment!</h2>
+      <p>Did you know that a 404 is the generic error code used across the internet to mean "page not found"? It might mean the page used to exist, but doesn't anymore. Or that someone made a typo somewhere. Regardless, there's no page at this address.</p>
+    </div>
+  );
+};
+
+module.exports = NotFoundMessage;
