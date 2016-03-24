8c7543afc626fb343b5246794d3fe8026d4de427
ScottDowne <scott.downe@gmail.com>
2016-03-24 11:07:15 -0400

    updating data and fixed a bug in comma separated links

--- pages/web-literacy/WebLiteracyPage.jsx
+++ pages/web-literacy/WebLiteracyPage.jsx
@@ -22,11 +22,11 @@ var makeLinksFromWebLitSkills = require("./MakeLinksFromWebLitSkills.jsx");
 function makeLinksFrom21CSkills(skills21C) {
   return skills21C.map(function(skill21C, index) {
     return (
-      <a key={skill21C} href={"/web-literacy/skills/#" + categories[skill21C]}>
-        <span className="comma-separated-links" key={skill21C}>
+      <span className="comma-separated-links">
+        <a className="comma-separated-links" key={skill21C} href={"/web-literacy/skills/#" + categories[skill21C]}>
           {categories[skill21C]}
-        </span>
-      </a>
+        </a>
+      </span>
     );
   });
 } 
--- pages/web-literacy/skillsdata.js
+++ pages/web-literacy/skillsdata.js
@@ -21,12 +21,58 @@ module.exports = [
     competencies: [
       "Identifying and defining a specific problem, challenge, or questions to investigate based on sound research and relevant data.",
       "Generating relevant questions based on observations.",
-      "Conducting independent research to develop hypothesis and research plan to solve the problem."
+      "Conducting independent research to develop hypothesis and research plan to solve the problem.",
+      "Proposing knowledge-based explanations to develop hypothesis and research plan to solve the problem.",
+      "Applying feedback, data, and research to revise and iterate on the plan as a part of the process."
     ],
     topics: {
       "Read": [search, navigate, synthesize, evaluate],
       "Write": [design, compose, code, revise, remix],
       "Participate": [share, contribute, connect, protect, open]
+    }
+  },
+  {
+    name: "Collaboration",
+    content: "Being audience and culturally aware, resolving conflict appropriately, using technology tools effectively, and taking responsibility for personal and group productivity.",
+    imgSrc1x: "/img/pages/web-literacy/21cskills/21c-skill-icon_collaboration.svg",
+    competencies: [
+      {
+        name: "Audience & Cultural Awareness",
+        content: [
+          "Engaging in behaviors and actions that show an awareness of and sensitivity to stereotyping and cultural bias.",
+          "Using language that is appropriate to the purpose of the interaction and audience.",
+          "Differentiating between appropriate and inappropriate behavior and actions to the purpose of the interaction and audience."
+        ]
+      },
+      {
+        name: "Conflict Resolution",
+        content: [
+          "Redirecting focus toward common ground and the task at hand.",
+          "Engaging with conflict in a way that strengthens overall coherence.",
+          "Negotiating shared understandings during conflict."
+        ]
+      },
+      {
+        name: "Use of Collaboration Technology",
+        content: [
+          "Participating in selection and utilization of technology tools  that enhance group productivity.",
+          "Following norms and conventions of communicating in online forums.",
+          "Demonstrating an understanding of the way technology impacts one’s own and others work or views."
+        ]
+      },
+      {
+        name: "Responsibility & Productivity",
+        content: [
+          "Preparing for obligations by reading, researching, and completing actions on time.",
+          "Submitting high-quality work products that meet goals.",
+          "Prioritizing and monitoring individual and team progress toward goals and making adjustments as needed.",
+          "Engaging in self-critique and self-reflection on strengths and areas of need.",
+          "Demonstrating ways that collaboration can lead to better productivity."
+        ]
+      }
+    ],
+    topics: {
+      "Participate": [share, contribute, connect, open]
     }
   },
   {
@@ -53,74 +99,32 @@ module.exports = [
     imgSrc1x: "/img/pages/web-literacy/21cskills/21c-skill-icon_communication.svg",
     competencies: [
       {
-        name: "placeholder 1",
+        name: "Message Development",
         content: [
           "Organizing presentation of ideas to appropriately inform and engage others.",
           "Choosing effective modes and methods of presentation to most effectively convey ideas.",
           "Evaluating one’s own interpretation and seeks data and evidence to enhance understanding of others.",
           "Discerning credible and relevant sources to bolster meaning and enhance understanding.",
           "Analyzing messages from others for implicit or explicit meaning to inform one’s own approach.",
-          "Evaluating and adjusting one’s own level of active engagement and degree of participation in group settings.",
-          "Seeking guidance to refine technique/approach to enhance effectiveness."
+          "Seeking guidance to refine technique/approach to enhance effectiveness.",
+          "Adjusting presentations based on anticipated and unanticipated challenges and/or feedback.",
+          "Writing with appropriate clarity, grammar and punctuation, and necessary information."
         ]
       },
       {
-        name: "placeholder 2",
+        name: "Group Contributions",
         content: [
           "Initiating opportunities to communicate, interact, and work positively with individuals and groups.",
           "Using appropriate tone, clarity, and styles of interacting with individuals and groups.",
           "Building upon or synthesizing the contributions of others in a way that facilitates contributions by others.",
-          "Offering alternative solutions that build on the ideas of others where appropriate.",
-          "Writing with appropriate clarity, grammar and punctuation, and necessary information."
+          "Evaluating and adjusting one’s own level of active engagement and degree of participation in group settings.",
+          "Offering alternative solutions that build on the ideas of others where appropriate."
         ]
       }
     ],
     topics: {
       "Read": [synthesize],
       "Write": [compose, remix],
-      "Participate": [share, contribute, connect, open]
-    }
-  },
-  {
-    name: "Collaboration",
-    content: "Interacting and working appropriately with diverse audiences and teams; demonstrating active listening, interacting and contributing constructively in group discussions and meetings; using appropriate technology tools for working together including resolving conflicts on group projects; and sharing responsibility for group work and valuing individual contributions.",
-    imgSrc1x: "/img/pages/web-literacy/21cskills/21c-skill-icon_collaboration.svg",
-    competencies: [
-      {
-        name: "Audience & Cultural Awareness",
-        content: [
-          "Engaging in behaviors and actions that show an awareness of and sensitivity to stereotyping and cultural bias.",
-          "Using language that is appropriate to the purpose of the interaction and audience.",
-          "Differentiating between appropriate and inappropriate behavior and actions to the purpose of the interaction and audience."
-        ]
-      },
-      {
-        name: "Conflict Resolution",
-        content: [
-          "Redirecting focus toward common ground and the task at hand.",
-          "Engaging with conflict in a way that strengthens overall coherence.",
-          "Negotiating shared understandings during conflict."
-        ]
-      },
-      {
-        name: "Use of Collaboration Technology",
-        content: [
-          "Participating in selection and utilization of technology tools  that enhance group productivity.",
-          "Following norms and conventions of communicating in online forums."
-        ]
-      },
-      {
-        name: "Responsibility & Productivity",
-        content: [
-          "Preparing for obligations by reading, researching, and completing actions on time.",
-          "Submitting high-quality work products that meet goals.",
-          "Prioritizing and monitoring individual and team progress toward goals and making adjustments as needed.",
-          "Engaging in self-critique and self-reflection on strengths and areas of need.",
-          "Demonstrating ways that collaboration can lead to better productivity."
-        ]
-      }
-    ],
-    topics: {
       "Participate": [share, contribute, connect, open]
     }
   }
