f3d671263a6183867fb8ac3f93ccf6f7b98c494e
alicoding <ali@alicoding.com>
2016-03-30 15:50:09 -0400

    stage

--- pages/wp-content.jsx
+++ pages/wp-content.jsx
@@ -4,11 +4,11 @@ var NotFoundMessage = require('../components/not-found.jsx');
 
 var config = require('../config/config');
 
-var WpContent = function(props) { 
+var WpContent = function(props) {
   return (
     <div className="wp-content-wrapper">
       <div className="inner-container">
-        <WpContentLoader wpUrl={config.WORDPRESS_SITE_URL} wpPostSlug={props.params.wpSlug}>
+        <WpContentLoader wpUrl={'alicoding.wordpress.com'} wpPostSlug={props.params.wpSlug}>
           <NotFoundMessage/>
         </WpContentLoader>
       </div>
