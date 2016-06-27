71701e2a48b552ca4845927f91a176515f75d6ed
Mavis Ou <mmmavisss@gmail.com>
2016-06-27 15:22:10 -0700

    Fixes #1910, Fixes #1740, tidy up newsletter signup form validation and tests

--- components/newsletter-signup/validateSignupForm.js
+++ components/newsletter-signup/validateSignupForm.js
@@ -1,9 +1,8 @@
-var util = require('../../lib/util');
+var validator = require('validator');
 
-// TODO: this is a really bad email validation, and should probably be removed.
 module.exports = function validateSignupForm(signUpFormState) {
   var errors = [];
-  if ( !util.isValidEmail(signUpFormState.email) ) {
+  if ( !validator.isEmail(signUpFormState.email) ) {
     errors.push("Please enter an email address.");
   }
   return errors;
