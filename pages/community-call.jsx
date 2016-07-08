5f32c35e01412917895d15d08f3180189a087547
hannahkane <hannah@mozillafoundation.org>
2016-07-08 12:35:53 -0700

    adds July content to landing page and archive page for June

--- pages/community-call.jsx
+++ pages/community-call.jsx
@@ -28,44 +28,40 @@ var CommunityCallPage = React.createClass({
 
           <section className="callout-box">
             <h2>Upcoming Call</h2>
-            <p className="date">June 29, 1pm PT / 4pm ET / 8pm UTC</p>
-            <h1>Learning and Making</h1>
+            <p className="date">July 27, 9am PT / 12pm ET / 4pm UTC</p>
+            <h1>Brokering Learning</h1>
             <p className="description">
-              Featured Speakers: Lisa Kim, Youth Commission Director for Mikva Challenge in Chicago, IL; Niberca (Gigi) Polo, Emoti-con 2016 design fellows lead, Part-Time Associate Teaching Professor at Parsons The New School for Design, and Principal at Myellow Boots Studio; Dr. Dixie Ching, co-lead of the Hive Research Lab; Andy Forest, lead at STEAMLabs.
+              Representing libraries, game education organizations, Mozilla Club leads and Mozilla staff, guests include: Hive Cascadia (Portland, Oregon, USA), Hive Toronto (Canada), Tina Verbo from the Mozilla Philippines community.
             </p>
           </section>
 
           <p>
-            Summer learning loss is a real challenge, especially for those who may not have access to enriching educational opportunities. There has been a good amount of research and discussion on this topic, and on the June community call, we’ll be exploring the types of fun and engaging learning experiences that can–and do–happen during the summer. Educators from Hive NYC, Hive Chicago and Hive Toronto will join us to share real examples of programs that illustrate the impact that learning and making can have on young people during this critical time of the year.
+            Learning has the potential to make global impact, especially when it starts local. During this month’s call, we’ll discuss how to develop successful learning opportunities that go beyond local borders. What is the recipe for small and large organizations to move their projects beyond city limits? We’ll discuss developing city-wide learning projects, including successes and challenges, and how they connect to the global Mozilla Learning networks for broader impact.
           </p>
 
           <p>
-            How do these summer programs Demystify the Web for young learners? Explore our learning space at the Mozilla Festival and how you can submit a proposal to share your program strategies and best practices with a community of like-minded peers in a professional learning environment that models hands-on, interest-driven learning for youth and adults alike.
-          </p>
-
-          <p>
-            Viewers can ask questions and share ideas and feedback of their own through the embedded agenda and chat.
+            Have an idea on how to scale a project globally? Ask questions, share ideas and feedback with us live through the embedded agenda and chat on the 27th!
           </p>
 
           <h3>Workshop Video Stream</h3>
 
           <div className="video-wrapper">
-            <iframe width="560" height="315" src="//www.youtube.com/embed/Uig7s0fChj8" frameBorder="0" allowFullScreen></iframe>
+            <iframe width="560" height="315" src="//www.youtube.com/embed/Elr7leEmfcs" frameBorder="0" allowFullScreen></iframe>
           </div>
 
           <h4>
             Open Agenda
-            <a title="Open the agenda in a new tab" className="fa fa-external-link open-etherpad" href="https://public.etherpad-mozilla.org/p/mozcommunitycallJune16">
+            <a title="Open the agenda in a new tab" className="fa fa-external-link open-etherpad" href="https://public.etherpad-mozilla.org/p/mozcommunitycallJuly16">
             </a>
           </h4>
 
-          <iframe className="etherpad" src="https://public.etherpad-mozilla.org/p/mozcommunitycallJune16"></iframe>
+          <iframe className="etherpad" src="https://public.etherpad-mozilla.org/p/mozcommunitycallJuly16"></iframe>
 
           <h2>Upcoming Calls</h2>
 
           <ul className="upcoming-workshops">
             <li>
-              <p className="date">July TBD</p>
+              <p className="date">August TBD</p>
               <h2>Theme: TBD</h2>
             </li>
           </ul>
@@ -73,6 +69,16 @@ var CommunityCallPage = React.createClass({
           <h2>Past Calls</h2>
 
           <ul className="past-workshops">
+            <li>
+              <p className="date">June 29, 2016</p>
+              <h2>Learning and Making</h2>
+              <p>
+                Summer learning loss is a real challenge, especially for those who may not have access to enriching educational opportunities. There has been a good amount of research and discussion on this topic, and on the June community call, we’ll be exploring the types of fun and engaging learning experiences that can–and do–happen during the summer. Educators from Hive NYC, Hive Chicago and Hive Toronto will join us to share real examples of programs that illustrate the impact that learning and making can have on young people during this critical time of the year.
+              </p>
+              <p className="watch-archive">
+                <LinkAnchorSwap to="/community/community-call/june-29-2016/">Watch the Replay</LinkAnchorSwap>
+              </p>
+            </li>
             <li>
               <p className="date">May 25, 2016</p>
               <h2>Gigabit</h2>
