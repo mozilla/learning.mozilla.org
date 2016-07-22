96e793fa206c54b7974236e910578f6c29ed1d1e
alicoding <ali@alicoding.com>
2016-07-22 09:26:11 -0400

    Localize about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -14,16 +14,13 @@ var Intro = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>About the Mozilla Learning Network</h1>
+        <h1>{this.context.intl.formatMessage({id: 'about_mln'})}</h1>
         <section className="intro">
           <Illustration width={226} height={226}
           src1x="/img/pages/about/hive-nyc-summer-quest.jpg"
           alt="HiveLearningNYC 2014 Hive NYC Summer Quest Maker Party">
             <p>
-              We are cultivating a global network of web literacy leaders who are teaching and
-              learning the most important skills of our age: the ability to read, write and
-              participate in the digital world. Our programs and resources can help you make
-              an impact in your community, and participate in an openly networked world.
+              {this.context.intl.formatMessage({id: 'about_mln_intro'})}
             </p>
           </Illustration>
         </section>
@@ -43,13 +40,10 @@ var OurFocus = React.createClass({
         src1x="/img/pages/home/svg/icon-circle-home.svg"
         alt="A graphic showing an illustration of a person, a book, some gears, a map pin, and a square acedemic cap">
           <ul className="colored-list">
-            <li>Learn and deepen your 21st Century digital skills, and get better at teaching them to others.</li>
-            <li>Contribute to real-world products and projects that are open, participatory and networked.</li>
-            <li>
-              Access free resources like event planning guides and step-by-step teaching activities, or share
-              your own resources to receive feedback from peers.
-            </li>
-            <li>Develop your skills in facilitative leadership, participatory practices and collaboration.</li>
+            <li>{this.context.intl.formatMessage({id: 'our_focus_one'})}</li>
+            <li>{this.context.intl.formatMessage({id: 'our_focus_two'})}</li>
+            <li>{this.context.intl.formatMessage({id: 'our_focus_three'})}</li>
+            <li>{this.context.intl.formatMessage({id: 'our_focus_four'})}</li>
           </ul>
         </Illustration>
       </section>
@@ -66,8 +60,7 @@ var OurGoal = React.createClass({
     <section className="row page-end our-goal">
       <div className="horizontal-divider"></div>
       <p className="learn-more">
-        In 2015, our goal is to foster and sustain web literacy activities in 500 cities around the globe.
-        Join us by teaching, learning, sharing and imagining the full power of the web.
+        {this.context.intl.formatMessage({id: 'our_goal'})}
       </p>
       <div className="horizontal-divider"></div>
     </section>
@@ -86,23 +79,23 @@ var IconLinksSets = React.createClass({
           link={config.TWITTER_LINK}
           imgSrc="/img/pages/about/svg/icon-twitter-blue.svg"
           width={60}
-          head="Follow Us"
-          subhead="Start a conversation on Twitter."
-          highlightedText="Twitter"
+          head={this.context.intl.formatMessage({id: 'follow_us'})}
+          subhead={this.context.intl.formatMessage({id: 'start_conv_on_twitter'})}
+          highlightedText={this.context.intl.formatMessage({id: 'twitter'})}
         />
         <IconLink
           link={"mailto:"+config.TEACH_THE_WEB_EMAIL}
           imgSrc="/img/pages/about/svg/icon-get-help-blue.svg"
-          head="Get Help"
-          subhead="Email us anytime."
-          highlightedText="Email us"
+          head={this.context.intl.formatMessage({id: 'get_help'})}
+          subhead={this.context.intl.formatMessage({id: 'email_us_anytime'})}
+          highlightedText={this.context.intl.formatMessage({id: 'email_us'})}
         />
         <IconLink
           link="https://discourse.webmaker.org/t/if-youre-new-to-the-community-please-introduce-yourself"
           imgSrc="/img/pages/about/svg/icon-connect-blue.svg"
-          head="Say Hello"
-          subhead="Connect on the Discourse forum."
-          highlightedText="Discourse forum"
+          head={this.context.intl.formatMessage({id: 'say_hello'})}
+          subhead={this.context.intl.formatMessage({id: 'connect_on_discourse'})}
+          highlightedText={this.context.intl.formatMessage({id: 'discourse_forum'})}
         />
       </IconLinks>
     )
