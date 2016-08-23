38a8b674afe66c92bd3269a3bdac585ba2f6c171
Pomax <pomax@nihongoresources.com>
2016-08-23 13:21:19 -0700

    less as npm task, rather than gulp

--- gulp/app.tasks.js
+++ gulp/app.tasks.js
@@ -5,7 +5,6 @@ var gutil = require('gulp-util');
 var indexStaticWatcher = require('../lib/build/index-static-watcher').create();
 var lightweightDynamicServer = require('../app');
 var MINIMAL_BUILD_TASKS = require('./shared/minimal.build.tasks.js');
-var LESS_FILES = require('./shared/less.files.js');
 var WATCH_DELAY = require('./shared/watch.delay.js');
 
 module.exports = function(gulp) {
@@ -21,8 +20,6 @@ module.exports = function(gulp) {
       lightweightDynamicServer.updateIndexStatic(newIndexStatic);
       console.log('Rebuilt server-side bundle.');
     });
-
-    gulp.watch(LESS_FILES, ['less']);
 
     lightweightDynamicServer.listen(config.DEV_SERVER_PORT, function() {
       gutil.log('Server defined by [app.js] listening at ' +
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 
 // define all tasks
 require('./gulp/copy.tasks.js')(gulp);
-require('./gulp/less.tasks.js')(gulp);
 require('./gulp/webpack.tasks.js')(gulp);
 require('./gulp/test.tasks.js')(gulp);
 require('./gulp/watch.tasks.js')(gulp);
