7b82fa429bf2a6c59f97ca2c4d1860536333e8cd
Alan Mooiman <amooiman@gmail.com>
2016-09-20 10:44:41 -0400

    Add new string specific to newsletter sign up
    Fixes #2173

--- components/newsletter-signup/SignupForm.jsx
+++ components/newsletter-signup/SignupForm.jsx
@@ -90,7 +90,7 @@ var SignupForm = React.createClass({
           </div>
           {this.renderValidationErrors()}
         </fieldset>
-        <input type="submit" value={this.context.intl.formatMessage({id: 'sign_up'})} className="btn" />
+        <input type="submit" value={this.context.intl.formatMessage({id: 'newsletter_sign_up'})} className="btn" />
       </form>
     );
   }
