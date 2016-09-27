d92e48d61bc65d70270f98a2262b8d15338f5983
Gavin Lazar Suntop <gavin@gsuntop.com>
2016-09-27 16:01:15 -0700

    fleshing out portfolio w cards

--- pages/gigabit/Portfolio.jsx
+++ pages/gigabit/Portfolio.jsx
@@ -3,7 +3,28 @@ var GigFoot = require('./GigFoot.jsx');
 var portfolioData = require('./portfolio.json');
 
 module.exports = React.createClass({
+  getInitialState() {
+    return {
+      projects: portfolioData
+    };
+  },
   render() {
+    var projects = this.state.projects.map((project, index) => {
+      return (
+        <div className={`col-md-4${index % 3 === 0 ? ` clear-left` : ``}`}>
+          <div className="project-card m-b-3">
+            <img className="photo" hidden={!project.Photo} src={project.Photo}/>
+            <div className="p-x-2 p-b-2">
+              <div className={`city m-b-0 ${project.Photo ? ` has-photo` : ` m-t-2`}`}>{project.City}</div>
+              <h3 className="m-y-0"><a className="project-name" href="#STUB">{project.Project}</a></h3>
+              <p className="lead-org">Lead Org: <a href="#">{project[`Lead Organization`]}</a></p>
+              <p className="summary">{project[`Project Summary`]}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div>
         <div className="text-center m-b-3">
@@ -13,22 +34,20 @@ module.exports = React.createClass({
 
         <div className="p-y-3 grey-block">
           <div className="text-center">
-            <h2>Find a Project</h2>
+            <div className="col-sm-8 col-sm-offset-2 m-b-3">
+              <h2 className="m-b-2">Find a Project</h2>
 
-            <input type="text" className="m-b-2"></input>
+              <input type="text" placeholder="Search for subject, location or technology" className="m-b-2"></input>
 
-            <div className="m-b-3">
-              <button>Featured Projects</button>
-              <button>Recently Updated</button>
-              <button>Recently Added</button>
+              <div className="m-b-2">
+                <button>Featured Projects</button>
+                <button>Recently Updated</button>
+                <button>Recently Added</button>
+              </div>
             </div>
 
-            <div className="m-b-3">
-              <p>PROJECTS GO HERE</p>
-            </div>
-
-            <div className="middle-button">
-              <button className="secondary-button" href="#">See More</button>
+            <div className="m-b-3 row">
+              {projects}
             </div>
           </div>
         </div>
