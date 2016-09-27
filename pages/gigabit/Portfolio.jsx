7115ee26821fe403e2cdefb540a9fa5a5fae434c
Gavin Lazar Suntop <gavin@gsuntop.com>
2016-09-27 10:24:31 -0700

    Portfolio scaffold

--- pages/gigabit/Portfolio.jsx
+++ pages/gigabit/Portfolio.jsx
@@ -5,7 +5,32 @@ module.exports = React.createClass({
   render() {
     return (
       <div>
-        <div>Portfolio</div>
+        <div className="text-center m-b-3">
+          <h2>Gigabit Project Portfolio</h2>
+          <p>Learn more about projects that are transforming education through gigabit technology.</p>
+        </div>
+
+        <div className="p-y-3 grey-block">
+          <div className="text-center">
+            <h2>Find a Project</h2>
+
+            <input type="text" className="m-b-2"></input>
+
+            <div className="m-b-3">
+              <button>Featured Projects</button>
+              <button>Recently Updated</button>
+              <button>Recently Added</button>
+            </div>
+
+            <div className="m-b-3">
+              <p>PROJECTS GO HERE</p>
+            </div>
+
+            <div className="middle-button">
+              <button className="secondary-button" href="#">See More</button>
+            </div>
+          </div>
+        </div>
 
         <GigFoot></GigFoot>
       </div>
