928d4f62f66062b451593e288990f52f41cc5e4d
hannahkane <hannah@mozillafoundation.org>
2016-12-06 20:00:34 -1000

    updates links and descriptions to Protect Your Data activities

--- pages/protect-your-data.jsx
+++ pages/protect-your-data.jsx
@@ -6,14 +6,13 @@ var ActivitySection = require('../components/activity-section.jsx');
 
 var Intro = (
   <div>
-    <h1>Protect Your Data</h1>
+    <h1>Privacy Basics: Protect Your Data</h1>
     <section className="intro">
       <Illustration
       height={204} width={204}
-      src1x="/img/pages/protect-your-data/photo-protect-your-data.png"
-      src2x="/img/pages/protect-your-data/photo-protect-your-data@2x.png"
-      alt="">
-        <h2>Engage learners around the topic of privacy in this six-part module, developed in conjunction with Hive Toronto and the Mozilla Privacy Team.</h2>
+      src1x="/img/pages/protect-your-data/ip-tracer-badge.jpg"
+      alt="hexoganal badge with magnifying glass icon">
+        <h2>These activities will help your learners understand how to take control of their privacy in a networked world. Learn web literacy skills like evaluate, navigate, open practice, and protect to safeguard your data and identity online.</h2>
       </Illustration>
     </section>
   </div>
@@ -24,7 +23,7 @@ var LearningObjectives = (
     <div className="col-sm-12">
       <h2>Learning Objectives</h2>
       <p>
-        These activities will help your learners understand how to take control of their privacy in a networked world. Complete the activities in sequence, or mix and match. Visit <a href="https://discourse.webmaker.org/c/curriculum">our discussion forum</a> to get help and share your experience.
+        Learners will understand how to create secure passwords, monitor their online privacy and security, protect their data, and identify online trackers using web literacy skills like <strong>evaluate</strong>, <strong>navigate</strong>, <strong>open practice</strong>, and <strong>protect</strong>.
       </p>
     </div>
   </section>
@@ -32,65 +31,53 @@ var LearningObjectives = (
 
 var sectionList = [
   {
-    title: "Reading the Web",
     activities: [
       {
         title: "IP Tracer",
         image1x: "/img/pages/protect-your-data/img-iptracer.png",
         image2x: "/img/pages/protect-your-data/img-iptracer@2x.png",
-        subtitle: "Understanding privacy",
-        description: "Learners explore internet protocol (IP) addresses and create a map to illustrate their associations with individuals, devices, or websites.",
-        link: "http://mozilla.github.io/webmaker-curriculum/ProtectingYourData/session01-ip.html"
+        subtitle: "Navigate, Search",
+        description: "You will explore the internet using internet protocol addresses - or IP addresses - to follow and map the connections between websites and devices, learning about <strong>navigate</strong> and <strong>search</strong>.",
+        link: "https://mozilla.github.io/curriculum-final/privacy-basics/session01-ip-tracer.html"
       },
       {
-        title: "Understanding security",
+        title: "Bad Password Collage",
         image1x: "/img/pages/protect-your-data/img-password.png",
         image2x: "/img/pages/protect-your-data/img-password@2x.png",
-        subtitle: "Understanding web mechanics",
-        description: "Learners investigate the differences between weak and strong passwords, and create a collage to document bad password practices.",
-        link: "http://mozilla.github.io/webmaker-curriculum/ProtectingYourData/session01-password-collage.html"
-      }
-    ]
-  },
-  {
-    title: "Writing the Web",
-    activities: [
+        subtitle: "Evaluate, Protect",
+        description: "You will learn about weak and strong passwords and test the strength of different passwords using tools called password meters. You will also make a &#34;bad password collage,&#34; learning web literacy skills like <strong>evaluate</strong> and <strong>protect</strong>.",
+        link: "https://mozilla.github.io/curriculum-final/privacy-basics/session02-bad-password-collage.html"
+      },
       {
         title: "Cookies and Third-Party Tracking",
         image1x: "/img/pages/protect-your-data/img-cookies.png",
         image2x: "/img/pages/protect-your-data/img-cookies@2x.png",
-        subtitle: "Understanding privacy and open practices",
-        description: "Using a free and open source tool called Mozilla Lightbeam, learners see how cookies and third-party trackers monitor their everyday tasks on the web. Then they divide into teams and race to see who can gather the most trackers.",
-        link: "http://mozilla.github.io/webmaker-curriculum/ProtectingYourData/session02-lightbeam.html"
+        subtitle: "Navigate, Open Practice, Protect",
+        description: "You will use a privacy tool called Mozilla Lightbeam to see how websites track your movement online, learning web literacy skills like <strong>navigate</strong>, <strong>open practice</strong>, and <strong>protect</strong>.",
+        link: "https://mozilla.github.io/curriculum-final/privacy-basics/session03-cookies-and-third-party-tracking.html"
       },
       {
-        title: "Draw Secure Passwords",
-        image1x: "/img/pages/protect-your-data/img-password.png",
-        image2x: "/img/pages/protect-your-data/img-password@2x.png",
-        subtitle: "Understanding privacy and security",
-        description: "Learners generate pass-phrases, pronounceable and random passwords based on best practices. Then they create a survey to pass along their knowledge to friends and family.",
-        link: "http://mozilla.github.io/webmaker-curriculum/ProtectingYourData/session02-secure-passwords.html"
-      }
-    ]
-  },
-  {
-    title: "Participating on the Web",
-    activities: [
+        title: "Create Secure Passwords",
+        image1x: "/img/pages/protect-your-data/pass-phrase.jpg",
+        subtitle: "Evaluate, Protect",
+        description: "You will learn several different ways to generate secure passwords and pass-phrases and test their strength, learning web literacy skills like <strong>evaluate</strong> and <strong>protect</strong>.",
+        link: "https://mozilla.github.io/curriculum-final/privacy-basics/session04-create-secure-passwords.html"
+      },
       {
         title: "Data Trail Timeline",
         image1x: "/img/pages/protect-your-data/img-datatrail.png",
         image2x: "/img/pages/protect-your-data/img-datatrail@2x.png",
-        subtitle: "Understanding remixing, privacy",
-        description: "Learners create a timeline, video or slideshow remix to demonstrate how information gets collected by companies and other organizations throughout the course of a typical day.",
-        link: "http://mozilla.github.io/webmaker-curriculum/ProtectingYourData/session03-datatrail.html"
+        subtitle: "Protect",
+        description: "You will create a timeline on a poster or in a video or slideshow to demonstrate how information about your movement in the real world gets collected online by companies and other organizations throughout the course of a typical day while learning about web literacy skills like <strong>protect</strong>.",
+        link: "https://mozilla.github.io/curriculum-final/privacy-basics/session05-data-trail-timeline.html"
       },
       {
         title: "Privacy Coach",
         image1x: "/img/pages/protect-your-data/img-privacycoach.png",
         image2x: "/img/pages/protect-your-data/img-privacycoach@2x.png",
-        subtitle: "Understanding privacy and open practices",
-        description: "Learners become privacy mentors to their peers by sharing their expertise through discussion or creating their own &ldquo;recipe&rdquo; documentation on privacy.",
-        link: "http://mozilla.github.io/webmaker-curriculum/ProtectingYourData/session03-privacycoach.html"
+        subtitle: "Open Practice, Protect",
+        description: "rYou will make a teaching and learning resource you can use to mentor your peers about online privacy and protecting their data while learning web literacy skills like <strong>open practice<strong> and <strong>protect</strong>.",
+        link: "https://mozilla.github.io/curriculum-final/privacy-basics/session06-privacy-coach.html"
       }
     ]
   }
