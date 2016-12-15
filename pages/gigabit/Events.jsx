24ce41741a7926e72164475756f2cdfe63d2e1f2
gavin lazar suntop <gavin@gsuntop.com>
2016-12-15 20:48:58 +0000

    removing mozfest from gigabit events (closes #2393) (#2398)

--- pages/gigabit/Events.jsx
+++ pages/gigabit/Events.jsx
@@ -14,21 +14,6 @@ module.exports = React.createClass({
           <div className="p-t-3 row">
             <div className="col-md-4">
               <div className="project-card m-b-3">
-                <a href="https://mozillafestival.org" target="_blank"><img className="photo" src="/img/pages/gigabit/events/mozfest.jpg"/></a>
-                <div className="p-x-2 p-b-2">
-                  <div className={`city tag tag-orange m-b-0 has-photo`}>October 28-30, 2016</div>
-                  <h3 className="m-y-0"><a className="project-name" href="https://mozillafestival.org" target="blank">Mozilla Festival 2016</a></h3>
-                  <p className="lead-org">Ravensbourne - 6 Penrose Way, Greenwich Peninsula, London SE10 0EW</p>
-                  <p className="summary">The world’s leading event for and by the open Internet movement. Join us to build, debate, and explore the future of our lives online.</p>
-                  <div className="middle-button">
-                    <a className="secondary-button" href="https://mozillafestival.org" target="_blank">See Event</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4">
-              <div className="project-card m-b-3">
                 <a href="http://sxswedu.com" target="_blank"><img className="photo" src="/img/pages/gigabit/events/sxsw.png"/></a>
                 <div className="p-x-2 p-b-2">
                   <div className={`city tag tag-orange m-b-0 has-photo`}>March 6-9, 2017</div>
